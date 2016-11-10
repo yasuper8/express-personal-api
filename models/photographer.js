a73572b5a0e3a5c159e2cc9afc3105a6060e2419
yasuper8 <ysuper8@gmail.com>
2016-11-10 15:56:52 -0800

    intial commit. adds schemas

---
+++ models/photographer.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+  var PhotographerSchema = new Schema({
+    name: String,
+    dateOfBirth: String,
+    location: String,
+    favoriteEquipment: String,
+    bio: String,
+    styleOfWorks: String,
+    alive: Boolean
+  });
+
+  var PhotoSchema = new Schema({
+    title: String,
+    datePublished: String,
+    description: String,
+    fStop: Number,
+    ShutterSpeed: Number,
+    photographer: {type: object.type.objectId, ref: 'Photographer'}
+  });
+
+  var Photographer = mongoose.model('Photographer', hotographerSchema);
+module.exports = Photographer;
+
+
+var Photo = mongoose.model('Photo', PhotoSchema);
--- server.js
+++ server.js
@@ -49,11 +49,15 @@ app.get('/api', function api_index(req, res) {
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
     documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    baseUrl: "http://rocky-hollows-55860.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api/photographers/:id", description: "Display one photographer"},
+      {method: "GET", path: "/api/photographers", description: "Display all photographers"},
+      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"},
+      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"}, // CHANGE ME
+      {method: "POST", path: "/api/photographers", description: "Create a new photographer"}
     ]
   })
 });
