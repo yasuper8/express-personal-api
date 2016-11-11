2bd1021ab819c4dc2ef74a5f3057c8818cd44e4c
yasuper8 <ysuper8@gmail.com>
2016-11-11 12:37:15 -0800

    adds handlebar template for search result data and profile. need to work on ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,4 +4,89 @@ $(document).ready(function(){
 
 // your code
 
+//Search form
+var $photographersSearch = $("#photographer-search");
+
+var $name = $("#name");
+
+var $result = $("#result");
+
+// loading gif
+var $loading = $('#loading');
+
+
+
+
+
+
+
+
+
+
+
+
+$photographersSearch.on('submit', function(event) {
+  event.preventDeafult();
+
+  //clears search box
+  $result.empty();
+
+  var searchName = $name.val();
+  if (searchName !== '') {
+      $loading.show();
+
+      $.ajax({
+        method: 'GET',
+        url: "api/photographers-list/:name",
+        success: handlePhotographersData // see this function defined below
+      });
+    } else {
+      // remind the user to enter a keyword
+      // one way is a "quick and ugly" alert
+      alert("Enter a name keyword to search!");
+    }
+
+    // reset the form
+    $spotifySearch[0].reset();
+
 });
+
+function handlePhotographersData(photographsResult) {
+  console.log("result response from photographers list", photographsResult);
+
+
+  var photographerFound = photographsResult.name;
+
+  if(photographsResult.length === 0) {
+      alert("There is no match with the name you input");
+    } else {
+
+
+      // console.log("trackResults :" trackResults.album.images[0].url)
+
+      // hide loading gif
+      $loading.hide();
+
+      var source = $("#photographers-template").html();
+      var template = Handlebars.compile(source);
+
+      //
+      // for(var i = 0; i < photographsResult.length; i++) {
+      //   var spotifyTemplate = {
+      //     albumUrl: trackResults[i].album.images[0].url,
+      //     songTitle: trackResults[i].album.name,
+      //     artistName: trackResults[i].artists[0].name,
+      //     previewUrl: trackResults[i].preview_url
+      //   }
+      //   var tempHtml = template(spotifyTemplate)
+      //   $('#results').append(tempHtml);
+      // }
+    }
+
+}
+
+
+
+
+
+});// end ready!
